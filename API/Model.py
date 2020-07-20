8c74e469eef27c20689dbb2ee6b9c3c0cd8d640b
Becky <rebecabivar@hotmail.com>
2020-07-20 12:56:53 -0300

    Update Model.py

--- API/Model.py
+++ API/Model.py
@@ -46,7 +46,7 @@ class Model:
         df_output = df_rec[['COD_CLIENTE','recommendedProducts']].drop_duplicates().sort_values('COD_CLIENTE').set_index('COD_CLIENTE')
 
         if print_csv:
-            df_output.to_csv(r'/var/www/recommendationSystem/API/output.csv', sep=';')
+            df_output.to_csv(r'output.csv', sep=';')
             print("An output file can be found with name 'output.csv'")
 
         return df_output
